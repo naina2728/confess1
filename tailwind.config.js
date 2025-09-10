cf05b575bd2f23446a2501bb42a079cdef2061b1
Nishu0 <itsthakkarnisarg@gmail.com>
2025-09-10 20:32:57 +0530

    tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      animation: {
+        'marquee': 'marquee 12s linear infinite',
+        'marquee-slow': 'marquee 24s linear infinite',
+      },
+      keyframes: {
+        marquee: {
+          '0%': { transform: 'translateX(0)' },
+          '100%': { transform: 'translateX(-50%)' },
+        }
+      }
+    },
+  },
+  plugins: [],
+}
